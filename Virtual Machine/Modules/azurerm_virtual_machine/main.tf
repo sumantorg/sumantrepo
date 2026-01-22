56147d58aea2de4dcdbf162b693a443cfd49ed29
manimittal <mittalmani07@gmail.com>
2026-01-22 19:50:05 +0530

    code added

---
+++ Virtual Machine/Modules/azurerm_virtual_machine/main.tf
@@ -0,0 +1,27 @@
+resource "azurerm_linux_virtual_machine" "ntas_linux_vm" {
+  for_each = var.vm_config
+  name                            = each.value.vm_name
+  resource_group_name             = each.value.resource_group_name
+  location                        = each.value.location
+  size                            = each.value.vm_size
+  admin_username                  = each.value.admin_username
+  admin_password                  = each.value.admin_password
+  disable_password_authentication = false
+  network_interface_ids = [
+    data.azurerm_network_interface.datanic[each.key].id
+  ]
+
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = each.value.publisher
+    offer     = each.value.offer
+    sku       = each.value.sku
+    version   = "latest"
+  }
+  #  custom_data = base64encode(file(var.custom_data))
+}
